5f9dfabd2f8c713ffa8c4035f1f2590515fb2fc1
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2023-08-25 08:32:51 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,10 +4,9 @@ Module 0-basic_cache
 """
 from base_caching import BaseCaching
 
-
 class BasicCache(BaseCaching):
     """
-    Overrides put() nad self()
+    Overrides put() and get()
     from parent
     """
 
@@ -15,10 +14,13 @@ class BasicCache(BaseCaching):
         """Add an item in the cache
         """
         if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # If cache is full, remove the oldest item
+                oldest_key = next(iter(self.cache_data))
+                del self.cache_data[oldest_key]
             self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key
         """
-        value = self.cache_data.get(key)
-        return value
+        return self.cache_data.get(key)
